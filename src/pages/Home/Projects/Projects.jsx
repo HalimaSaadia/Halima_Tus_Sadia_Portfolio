ee889277715838828424b6453d69d40f230731f0
HalimaSaadia <halimasadia9666@gmail.com>
2024-08-31 21:37:06 +0600

    added drop shadow in nexus news project

--- src/pages/Home/Projects/Projects.jsx
+++ src/pages/Home/Projects/Projects.jsx
@@ -75,74 +75,80 @@ const Projects = () => {
                 </ul>
               </div>
             </div>
-           
+
             <div className="px-5 md:px-0 order-first flex items-center lg:order-last">
               <div data-aos="fade-up-left" data-aos-once="false">
-              <img style={{filter: "drop-shadow(5px 5px 10px #98c1d9)"}} src={dreamFinder} className="h-[95%] mb-10 lg:mb-0 w-full drop-shadow-2xl shadow-blue-500" alt="" />
+                <img
+                  style={{ filter: "drop-shadow(5px 5px 10px #98c1d9)" }}
+                  src={dreamFinder}
+                  className="h-[95%] mb-10 lg:mb-0 w-full drop-shadow-2xl shadow-blue-500"
+                  alt=""
+                />
               </div>
             </div>
-           
           </div>
         </div>
         <div id="" className=" relative w-full">
           <div className="grid  lg:grid-cols-2 pb-16 pt-8 mb-5 space-y-5 lg:space-y-0 lg:space-x-5 xl:space-x-0">
-            <div data-aos="fade-up-left"   data-aos-once="false">
-            <div className="  px-5">
-              <h3 className="text-2xl md:text-3xl font-semibold text-white mb-5">
-                <span className="text-[#ee6c4d]  font-bold">NexusNews </span>
-                <span className="text-xl md:text-2xl">
-                  ( FULLSTACK NEWSPAPER WEBSITE )
-                </span>
-              </h3>
-              <ul className="space-y-3">
-                <li className="">
-                  <span className="font-bold text-white">
-                    {" "}
-                    Comprehensive user management system:
-                  </span>{" "}
-                  Users can view all articles, add, edit, and delete their own
-                  articles. They can also subscribe to the website for different
-                  periods of time and update their profile information.
-                </li>
-                <li className="">
-                  <span className="font-bold text-white">
-                    {" "}
-                    Powerful admin dashboard:
-                  </span>{" "}
-                  Admins can manage user role, approve or decline and delete
-                  articles, and add new publishers.
-                </li>
-                <li className="">
-                  <span className="font-bold text-white">
-                    {" "}
-                    Security and technology:
-                  </span>{" "}
-                  User authentication is implemented using Firebase, and
-                  authorization is managed using JWTs. React, material UI used
-                  in front-end and express JS and mongoDB used in back-end.
-                </li>
-                <li className="">
-                  <span className="font-bold text-white">
-                    {" "}
-                    <FaGlobeAmericas className="inline" /> &nbsp;Website Link :
-                  </span>{" "}
-                  <a href="https://nexusnewsbd.netlify.app">
-                    https://nexusnewsbd.netlify.app
-                  </a>
-                </li>
-                <li className="">
-                  <span className="font-bold text-white">
-                    <GrTechnology className="inline" /> &nbsp;Technologies :{" "}
-                  </span>{" "}
-                  React JS, Material UI, Node JS, Express JS, MongoDB, Firebase
-                  etcetera.
-                </li>
-              </ul>
-            </div>
+            <div data-aos="fade-up-left" data-aos-once="false">
+              <div className="  px-5">
+                <h3 className="text-2xl md:text-3xl font-semibold text-white mb-5">
+                  <span className="text-[#ee6c4d]  font-bold">NexusNews </span>
+                  <span className="text-xl md:text-2xl">
+                    ( FULLSTACK NEWSPAPER WEBSITE )
+                  </span>
+                </h3>
+                <ul className="space-y-3">
+                  <li className="">
+                    <span className="font-bold text-white">
+                      {" "}
+                      Comprehensive user management system:
+                    </span>{" "}
+                    Users can view all articles, add, edit, and delete their own
+                    articles. They can also subscribe to the website for
+                    different periods of time and update their profile
+                    information.
+                  </li>
+                  <li className="">
+                    <span className="font-bold text-white">
+                      {" "}
+                      Powerful admin dashboard:
+                    </span>{" "}
+                    Admins can manage user role, approve or decline and delete
+                    articles, and add new publishers.
+                  </li>
+                  <li className="">
+                    <span className="font-bold text-white">
+                      {" "}
+                      Security and technology:
+                    </span>{" "}
+                    User authentication is implemented using Firebase, and
+                    authorization is managed using JWTs. React, material UI used
+                    in front-end and express JS and mongoDB used in back-end.
+                  </li>
+                  <li className="">
+                    <span className="font-bold text-white">
+                      {" "}
+                      <FaGlobeAmericas className="inline" /> &nbsp;Website Link
+                      :
+                    </span>{" "}
+                    <a href="https://nexusnewsbd.netlify.app">
+                      https://nexusnewsbd.netlify.app
+                    </a>
+                  </li>
+                  <li className="">
+                    <span className="font-bold text-white">
+                      <GrTechnology className="inline" /> &nbsp;Technologies :{" "}
+                    </span>{" "}
+                    React JS, Material UI, Node JS, Express JS, MongoDB,
+                    Firebase etcetera.
+                  </li>
+                </ul>
+              </div>
             </div>
             <div className="px-5 md:px-0 order-first  flex items-center">
-              <div data-aos="fade-up-right"   data-aos-once="false">
-              <img src={nexusNews} className="mb-10 lg:mb-0" alt="" />
+              <div data-aos="fade-up-right" data-aos-once="false">
+                <img style={{filter: "drop-shadow(5px 5px 10px #98c1d9)"}} src={nexusNews} className="mb-10 lg:mb-0" alt="" />
               </div>
             </div>
           </div>
@@ -151,69 +157,72 @@ const Projects = () => {
         <div id="" className=" relative w-full">
           <div className="grid lg:grid-cols-2 py-12 mb-5  lg:space-x-5 xl:space-x-0">
             <div className="px-5 md:px-0 order-first lg:order-last ">
-             <div data-aos="fade-up-left"   data-aos-once="false">
-             <img
-                src={ServiceSquad}
-                className="w-full sm:w-auto mx-auto"
-                alt=""
-              />
-             </div>
+              <div data-aos="fade-up-left" data-aos-once="false">
+                <img
+                  src={ServiceSquad}
+                  className="w-full sm:w-auto mx-auto"
+                  alt=""
+                />
+              </div>
             </div>
-           <div data-aos="fade-up-up-right" data-aos-once="false">
-           <div className="  px-5 ">
-              <h3 className="text-2xl md:text-3xl font-semibold text-white mb-5">
-                <span className="text-[#ee6c4d]  font-bold">ServiceSquad </span>
-                <span className="text-xl md:text-2xl">
-                  {" "}
-                  (OFFLINE SERVICE PROVIDING WEBSITE)
-                </span>
-              </h3>
-              <ul className="space-y-3 text-justify">
-                <li className="">
-                  <span className="font-bold text-white">
+            <div data-aos="fade-up-up-right" data-aos-once="false">
+              <div className="  px-5 ">
+                <h3 className="text-2xl md:text-3xl font-semibold text-white mb-5">
+                  <span className="text-[#ee6c4d]  font-bold">
+                    ServiceSquad{" "}
+                  </span>
+                  <span className="text-xl md:text-2xl">
                     {" "}
-                    User-empowered service management:
-                  </span>{" "}
-                  Users can take any service, add, update, and delete their own
-                  services.
-                </li>
-                <li className="">
-                  <span className="font-bold text-white">
-                    {" "}
-                    Real-time service status tracking:
-                  </span>{" "}
-                  Users can monitor and change the status of their services,
-                  whether pending, in progress, or completed, ensuring timely
-                  service delivery. Users can also view their booked services.
-                </li>
-                <li className="">
-                  <span className="font-bold text-white">
-                    {" "}
-                    Security and technology:
-                  </span>{" "}
-                  User authentication is implemented using Firebase, and
-                  authorization is managed using JWTs. React, material UI used
-                  in front-end and express JS and mongoDB used in back-end.
-                </li>
-                <li className="">
-                  <span className="font-bold text-white">
-                    {" "}
-                    <FaGlobeAmericas className="inline" /> &nbsp;Website Link :
-                  </span>{" "}
-                  <a href="https://service-squad.netlify.app">
-                    https://service-squad.netlify.app
-                  </a>
-                </li>
-                <li className="">
-                  <span className="font-bold text-white">
-                    <GrTechnology className="inline" /> &nbsp;Technologies :{" "}
-                  </span>{" "}
-                  React JS, Daisy UI, Tailwind CSS, Node JS, MongoDB, Firebase
-                  etcetera.
-                </li>
-              </ul>
+                    (OFFLINE SERVICE PROVIDING WEBSITE)
+                  </span>
+                </h3>
+                <ul className="space-y-3 text-justify">
+                  <li className="">
+                    <span className="font-bold text-white">
+                      {" "}
+                      User-empowered service management:
+                    </span>{" "}
+                    Users can take any service, add, update, and delete their
+                    own services.
+                  </li>
+                  <li className="">
+                    <span className="font-bold text-white">
+                      {" "}
+                      Real-time service status tracking:
+                    </span>{" "}
+                    Users can monitor and change the status of their services,
+                    whether pending, in progress, or completed, ensuring timely
+                    service delivery. Users can also view their booked services.
+                  </li>
+                  <li className="">
+                    <span className="font-bold text-white">
+                      {" "}
+                      Security and technology:
+                    </span>{" "}
+                    User authentication is implemented using Firebase, and
+                    authorization is managed using JWTs. React, material UI used
+                    in front-end and express JS and mongoDB used in back-end.
+                  </li>
+                  <li className="">
+                    <span className="font-bold text-white">
+                      {" "}
+                      <FaGlobeAmericas className="inline" /> &nbsp;Website Link
+                      :
+                    </span>{" "}
+                    <a href="https://service-squad.netlify.app">
+                      https://service-squad.netlify.app
+                    </a>
+                  </li>
+                  <li className="">
+                    <span className="font-bold text-white">
+                      <GrTechnology className="inline" /> &nbsp;Technologies :{" "}
+                    </span>{" "}
+                    React JS, Daisy UI, Tailwind CSS, Node JS, MongoDB, Firebase
+                    etcetera.
+                  </li>
+                </ul>
+              </div>
             </div>
-           </div>
           </div>
         </div>
       </div>
