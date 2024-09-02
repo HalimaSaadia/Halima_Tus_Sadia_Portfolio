a39bf3bd65338e7c7b0df76813f9c7a05997158f
HalimaSaadia <halimasadia9666@gmail.com>
2024-09-02 23:00:48 +0600

    added icon in github icon

--- src/pages/Home/Skills/Skills.jsx
+++ src/pages/Home/Skills/Skills.jsx
@@ -294,7 +294,7 @@ const Skills = () => {
           <div className="card  shadow-xl rounded-none">
             <div className="card-body">
               <div className="flex items-center">
-                <div className=" max-w-fit p-3 rounded-full shadow-2xl  ">
+                <div className=" max-w-fit p-3 rounded-full shadow-2xl  border border-gray-700">
                   <FaGithub className="text-5xl text-black bg-white rounded-full skillBox" />
                 </div>
                 <div className="px-3 flex-1">
