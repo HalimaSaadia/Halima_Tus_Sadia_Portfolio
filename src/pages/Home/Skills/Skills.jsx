ef23616e5402a88a6578825fc3734ad86a7e78b9
HalimaSaadia <halimasadia9666@gmail.com>
2024-09-02 22:29:03 +0600

    added border in javascript icon

--- src/pages/Home/Skills/Skills.jsx
+++ src/pages/Home/Skills/Skills.jsx
@@ -86,7 +86,7 @@ const Skills = () => {
           <div className="card  shadow-xl rounded-none">
             <div className="card-body">
               <div className="flex items-center">
-                <div className=" max-w-fit p-3 rounded-full  shadow-2xl  ">
+                <div className=" max-w-fit p-3 rounded-full  shadow-2xl border border-gray-700 ">
                   <IoLogoJavascript className="text-5xl text-[#E8D44D] skillBox rounded-full" />
                 </div>
                 <div className="px-3 flex-1">
