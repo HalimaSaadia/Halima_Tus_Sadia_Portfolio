8f51949c77234d7bd0b74e2a9024513d9cc2f58b
HalimaSaadia <halimasadia9666@gmail.com>
2024-09-02 23:03:52 +0600

    adde icon in html icon

--- src/pages/Home/Skills/Skills.jsx
+++ src/pages/Home/Skills/Skills.jsx
@@ -346,7 +346,7 @@ const Skills = () => {
           <div className="card  shadow-xl rounded-none">
             <div className="card-body">
               <div className="flex items-center">
-                <div className=" max-w-fit p-3 rounded-full shadow-2xl  ">
+                <div className=" max-w-fit p-3 rounded-full shadow-2xl border border-gray-700 ">
                   <FaHtml5 className="text-5xl text-[#DD4B25] skillBox" />
                 </div>
                 <div className="px-3 flex-1">
