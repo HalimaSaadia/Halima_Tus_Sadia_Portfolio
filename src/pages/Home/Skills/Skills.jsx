0bac8841ba58e3e1cce92d6a6dd62096a985f2ac
HalimaSaadia <halimasadia9666@gmail.com>
2024-09-02 23:01:18 +0600

    added icon in daisy ui icon

--- src/pages/Home/Skills/Skills.jsx
+++ src/pages/Home/Skills/Skills.jsx
@@ -268,7 +268,7 @@ const Skills = () => {
           <div className="card  shadow-xl rounded-none">
             <div className="card-body">
               <div className="flex items-center">
-                <div className=" max-w-fit p-3 rounded-full shadow-2xl  ">
+                <div className=" max-w-fit p-3 rounded-full shadow-2xl border border-gray-700 ">
                   <SiDaisyui className="text-5xl text-[#1AD1A5] skillBox" />
                 </div>
                 <div className="px-3 flex-1">
