8e08a869cfbd2fc3cd0fdf957eb49501c85c7d00
HalimaSaadia <halimasadia9666@gmail.com>
2024-09-05 10:32:19 +0600

    added border in tailwind skill

--- src/pages/Home/Skills/Skills.jsx
+++ src/pages/Home/Skills/Skills.jsx
@@ -212,7 +212,7 @@ const Skills = () => {
           <span className="left absolute"></span>
         </div>
 
-        <div data-aos-duration="3000" data-aos="zoom-in-right" className="hover:scale-110 ease-in-out duration-500 hover:shadow-xl hover:shadow-[#98c1d9]/50 relative">
+        <div data-aos-duration="3000" data-aos="zoom-in-right" className="hover:scale-110 ease-in-out duration-500 hover:shadow-xl hover:shadow-[#98c1d9]/50 relative border border-gray-700 rounded-lg">
           <div className="card  shadow-xl rounded-none">
             <div className="card-body">
               <div className="flex items-center">
